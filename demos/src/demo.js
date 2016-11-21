656b8280c8db6bb2535fd525dbeb5c74d1b7f145
alicebartlett <alice.bartlett@gmail.com>
2016-11-21 15:13:21 +0000

    Get demo code working

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -3,6 +3,7 @@ import './../../main.js';
 
 function initDemos() {
 	document.addEventListener('DOMContentLoaded', function() {
+		document.documentElement.className = document.documentElement.className.replace('core', 'enhanced');
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 	});
 }
